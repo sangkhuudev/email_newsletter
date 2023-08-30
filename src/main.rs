6d6cfedb70780d541b57d1c890385fc963c0c60c
Khuu Viet Sang <sangkhuudev@gmail.com>
2023-08-30 13:04:19 +0700

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use email_newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
+}
+
